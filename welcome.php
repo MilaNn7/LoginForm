67f6e2ec6b8e96d455a8cd2be4a2737270ca4507
MilaNn7 <134263914+MilaNn7@users.noreply.github.com>
2024-04-23 13:34:55 +0200

    welcome.php

--- welcome.php
+++ welcome.php
@@ -6,7 +6,7 @@ if(isset($_SESSION['username']) ) {
     echo '<link rel=stylesheet href="welcome.css">';
     echo '<div class="zaklad">';
     echo '<p>Welcome <span>'.$_SESSION['username'].'</span></p><br>';
-    echo '<p>Click here to <a href = "logout.php" tite = "Logout">logout.</p>';//odkaz na odhlasenie
+    echo '<p>Click here to <a href = "logout.php" tite = "Logout">logout. </a></p>';//odkaz na odhlasenie
     
     $servername = "localhost";
     $username = "horvath3a2";
